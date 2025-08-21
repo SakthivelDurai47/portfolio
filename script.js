d801563f9995d96f41d21fd337cbe4f5a0ef00ff
Sakthivel Durai <sakthiveldurai47@gmail.com>
2025-08-21 22:27:42 +0530

    almost finished the structure

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 const sections = document.querySelectorAll(".section");
 const navLinks = document.querySelectorAll("nav a");
+const title = document.getElementById("title-name");
 
 const observer = new IntersectionObserver(
   (entries) => {
     entries.forEach((entry) => {
       if (entry.isIntersecting) {
-        // Remove active from all
         navLinks.forEach((link) => link.classList.remove("active"));
 
-        // Add active to the matching link
         const id = entry.target.id;
         const activeLink = document.querySelector(`nav a[href="#${id}"]`);
-        if (activeLink) activeLink.classList.add("active");
+        // if (id == "home") {
+        //   title.style.display = "none";
+        // } else {
+        //   title.style.display = "block";
+        // }
+        if (activeLink) {
+          activeLink.classList.add("active");
+        }
       }
     });
   },
-  { threshold: 0.6 }
-); // means: 60% of element must be visible
+  { threshold: 0.5 }
+);
 
 sections.forEach((section) => observer.observe(section));
